2e610ba220ff3bb9624895217db501be5818707e
hhchang0210 <42891908+hhchang0210@users.noreply.github.com>
2025-08-05 19:30:18 +0800

    01
    設定自己的 stack
    使用uart_puts來印出字串

--- code/os/01-helloRVOS/uart.c
+++ code/os/01-helloRVOS/uart.c
@@ -6,6 +6,8 @@
  * This macro returns the address of one of the registers.
  */
 #define UART_REG(reg) ((volatile uint8_t *)(UART0 + reg))
+// UART0 的位置 #define UART0 0x10000000L
+// 加上 UART0 的位置當作 reg 的位置
 
 /*
  * Reference
@@ -61,8 +63,10 @@
  * 1 = transmitter hold register (or FIFO) is empty. CPU can load the next character.
  * ......
  */
+// LSR 的第五個 bit 為 1 的話, cpu 就可以讀下一個字元
 #define LSR_RX_READY (1 << 0)
 #define LSR_TX_IDLE  (1 << 5)
+
 
 #define uart_read_reg(reg) (*(UART_REG(reg)))
 #define uart_write_reg(reg, v) (*(UART_REG(reg)) = (v))
@@ -70,7 +74,11 @@
 void uart_init()
 {
 	/* disable interrupts. */
+	/*初始化階段: 在設定 UART 參數時，避免在設定完成前產生不必要的中斷。
+	  透過將 IER 暫存器清零，來達到 完全關閉所有 UART 中斷 的目的。
+	*/
 	uart_write_reg(IER, 0x00);
+
 
 	/*
 	 * Setting baud rate. Just a demo here if we care about the divisor,
@@ -88,6 +96,15 @@ void uart_init()
 	 * split the value of 3(0x0003) into two bytes, DLL stores the low byte,
 	 * DLM stores the high byte.
 	 */
+
+	 /*
+	 我們不確定 LCR 的初始值，所以必須先讀取它，以避免覆蓋掉之前設定好的其他參數（例如資料位元數、停止位元數等）。
+	 uart_write_reg(LCR, lcr | (1 << 7)); 這行程式碼會設定 LCR 的第 7 個位元 (bit 7) 為 1。這會確保 LCR 的第 7 個位元被設定為 1，同時保留其他位元的值不變。
+	 LCR 的第 7 位元稱為 DLAB (Divisor Latch Access Bit)。
+	 當 DLAB = 1 時，CPU 讀寫 DL (Divisor Latch) 暫存器時，實際上是在操作 DLL (Divisor Latch Low) 和 DLM (Divisor Latch High) 這兩個暫存器。
+     當 DLAB = 0 時，CPU 讀寫 DL 暫存器時，實際上是在操作 IER 和 FCR (FIFO Control Register) 等暫存器。
+     因此，這行程式碼的目的是開啟 DLAB，讓後續的 DLL 和 DLM 寫入操作能夠真正影響到波特率的設定。
+	 */
 	uint8_t lcr = uart_read_reg(LCR);
 	uart_write_reg(LCR, lcr | (1 << 7));
 	uart_write_reg(DLL, 0x03);
@@ -101,13 +118,35 @@ void uart_init()
 	 * - no break control
 	 * - disabled baud latch
 	 */
+	/*
+	程式碼 (3 << 0) 的結果是 0x03，其二進位表示為 00000011。
+    這會將 LCR 的第 0 和第 1 個位元都設定為 1 (11)。
+    根據 LCR 的規範，LCR[1:0] = 11 代表將資料位元數設定為 8 個位元。
+	*/
 	lcr = 0;
 	uart_write_reg(LCR, lcr | (3 << 0));
-}
 
+}
+/*
+
+uart_putc 的功能是同步 (synchronous) 地傳送一個字元。它採用了忙碌等待 (busy-waiting) 的方式：
+等待：透過一個空迴圈不斷檢查 UART 的狀態。
+傳送：當 UART 準備好時，將字元寫入傳送暫存器。
+這種方式雖然簡單直觀，但在等待期間，CPU 會一直處於忙碌狀態，無法執行其他任務。在更進階的系統中，通常會使用中斷 (interrupt) 來取代忙碌等待，讓 CPU 在等待時可以執行其他工作，提高系統的效率。
+*/
 int uart_putc(char ch)
 {
+	/*
+	這個 while 迴圈會持續地、重複地讀取 LSR 暫存器，直到 LSR_TX_IDLE 位元變成 1 為止。這是一個等待的過程，
+	確保在寫入字元之前，UART 的傳送緩衝區已經是空的，避免資料遺失。
+	*/
 	while ((uart_read_reg(LSR) & LSR_TX_IDLE) == 0);
+
+	/*
+	THR 是 UART 專門用來存放待傳送資料的暫存器。當你將字元寫入 THR 後，UART 硬體就會自動開始將這個字元轉換為串行訊號並發送出去。
+    函式會回傳 uart_write_reg 的執行結果，通常這個函式會回傳 0 或其他代表成功的值
+	
+	*/
 	return uart_write_reg(THR, ch);
 }
 
